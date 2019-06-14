9911cd88ec330b4d91d623823faa9641e8d822d8
Adrian Gomez <adriangomez2099@gmail.com>
2019-06-14 04:48:03 -0600

    Update Cliente_A_J.py

--- Cliente_A_J.py
+++ Cliente_A_J.py
@@ -45,6 +45,7 @@ class NodeMCU(Thread):
     #           ___________________________           
     #__________/Constructor de la clase
     def __init__(self, ip = '192.168.0.200', port = 7070):
+    #def __init__(self, ip = '192.168.43.24', port = 7070):
         Thread.__init__(self)
         self.node_address = (ip,port)
         
