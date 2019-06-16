b598e86d5585c77fad9dc153dce1e6c751efdb7e
Jjvv21 <47864787+Jjvv21@users.noreply.github.com>
2019-06-15 22:16:02 -0600

    update 22:15...

--- Visual_Porsche/testdrive.py
+++ Visual_Porsche/testdrive.py
@@ -7,13 +7,24 @@ import threading
    
 root=Tk()
 root.title("Formula 1 - ADJ")
-root.config(bg="gray",width=1485,height=900)
+root.config(bg="black",width=1485,height=900)
 root.resizable(width=0,height=0)
 fondo=PhotoImage(file='porsche_dash.gif')
 label_de_imagen=Label(root,image=fondo).place(x=0,y=0)
 icono=root.iconbitmap('f1logo.ico')
+
+## VOLANTE EN POSICION NORMAL ## 
 volante=PhotoImage(file="volante_0.png")
-L_volante=Label(root,image=volante).place(x=1202,y=0)
+## VOLANTE EN POSICION 50 DERECHA ##
+volante2=PhotoImage(file='volante_d_50.png')
+## VOLANTE EN POSICION 90 DERECHA ##
+volante3=PhotoImage(file='volante_d_90.png')
+## VOLANTE EN POSICION 50 IZQUIERDA ##
+volante4=PhotoImage(file='volante_i_50.png')
+## VOLANTE EN POSICION 90 IZQUIERDA ##
+volante5=PhotoImage(file='volante_i_90.png')
+L_volante=Label(root,image=volante)
+L_volante.place(x=1202,y=0)
 
 dir_default=PhotoImage(file="direccion_izquierda_default.png")        
 direccional_default=Label(root,image=dir_default).place(x=1202,y=500)
@@ -22,22 +33,24 @@ dir_derecha_default=PhotoImage(file="direccion_derecha_default.png")
 direccional_derecha_default=Label(root,image=dir_derecha_default).place(x=1340,y=500)
 
 Escuderia=Label(root,justify=CENTER,text="PROPORSCHE2k19",font=("Comic Sans MS",7),relief="sunken",bg="black",fg="white").place(x=746,y=244)
- 
+def r(event):
+    print("hola")
+root.bind("<Right>",r)
 
     ### botones con imagenes ###
 pedal_closh=PhotoImage(file="pedal.png")
-closh=Button(root,image=pedal_closh).place(x=245,y=440)
+closh=Button(root,bd=0,image=pedal_closh).place(x=245,y=440)
 pedal_freno=PhotoImage(file="freno.png")
-freno=Button(root,image=pedal_freno).place(x=400,y=440)
+freno=Button(root,bd=0,image=pedal_freno).place(x=400,y=440)
 
 pedal_gas=PhotoImage(file="gas.png")
-gas=Button(root,image=pedal_gas).place(x=514,y=440)
+gas=Button(root,bd=0,image=pedal_gas).place(x=514,y=440)
 
 palanca_direccional=PhotoImage(file="direccional.png")
-direccional_izquierda=Button(root,image=palanca_direccional,command=lambda :thread_direccion_izquierda()).place(x=275,y=298)
+direccional_izquierda=Button(root,bd=0,image=palanca_direccional,command=lambda :thread_direccion_izquierda()).place(x=275,y=298)
 
 palanca_direccional2=PhotoImage(file="direccional_derecha.png")
-direccional_derecha=Button(root,image=palanca_direccional2,command=lambda :thread_direccion_derecha()).place(x=515,y=298)
+direccional_derecha=Button(root,bd=0,image=palanca_direccional2,command=lambda :thread_direccion_derecha()).place(x=515,y=298)
 
 
 ### BOTONES DE MOVIMIENTOS ESPECIALES Y FUNCION ##
@@ -51,10 +64,20 @@ celebracion=Button(root,image=ganador).place(x=1358,y=282)
 mov_especial=PhotoImage(file='movimiento_especial_apagado.png')
 especial=Button(root,image=mov_especial,command=lambda: thread_especial()).place(x=955,y=710)
        
+#BOTONES DE DIRECCION #
 
-        
-                        
-    
+
+## GIRAR HACIA LA DERECHA ### 
+girar_D=PhotoImage(file='girar_derecha.png')
+girar_derecha=Button(root,bd=0,image=girar_D,command=lambda: thread_girar_derecha()).place(x=1385,y=600)
+
+### GIRAR HACIA LA IZQUIERDA ### 
+girar_I=PhotoImage(file='girar_izquierda.png')
+girar_izquierda=Button(root,bd=0,image=girar_I,command=lambda: thread_girar_izquierda()).place(x=1203,y=600)
+
+### PONER EN DIRECCION 0 ####     
+direccion_0=PhotoImage(file='direccion_0.png')
+girar_none=Button(root,bd=0,image=direccion_0,command=lambda: thread_posicion_0()).place(x=1310,y=600)
 
 ### CIRCULO ###
 
@@ -93,7 +116,7 @@ ir_atras=Button(root,image=flecha_abajo).place(x=865,y=367)
 ################## FUNCIONES PARA LOS BOTONES Y LABELS ##########################################################################################################
 #### FUNCION PARA LAS LUCES FRONTALES Y LOS BOTONES  ##########
 def btn_hide():
-    if b1.winfo_ismapped()== True:
+    if b1.winfo_ismapped():
         b2.place_forget()
         b1.place(x=1202,y=282)
        
@@ -102,7 +125,7 @@ def btn_hide():
         b1.place(x=1202, y=282)
         
 def btn_show():
-    if b2.winfo_ismapped()== True:
+    if b2.winfo_ismapped():
         b1.place_forget()
         b2.place(x=1202,y=282)
        
@@ -257,7 +280,84 @@ def especial_cambio():
 def thread_especial():
     p=threading.Thread(target=especial_cambio)
     p.start()
+
     
+### HILO PARA LA POSICION 1 ####          
+def girar_derecha():
+        global volante,girar_D
+        x=0
+        while(x<1):
+            girar_D=PhotoImage(file='girar_derecha_press.png')
+            girar_derecha=Button(root,bd=0,image=girar_D,command=lambda: thread_girar_derecha()).place(x=1385,y=600)
+            volante=PhotoImage(file='volante_0.png')
+            L_volante=Label(root,image=volante).place(x=1202,y=0)
+            time.sleep(0.5)
+            volante=PhotoImage(file="volante_d_50.png")
+            L_volante=Label(root,image=volante).place(x=1202,y=0)
+            time.sleep(0.5)
+            girar_D=PhotoImage(file='girar_derecha.png')
+            girar_derecha=Button(root,bd=0,image=girar_D,command=lambda: thread_girar_derecha()).place(x=1385,y=600)
+            volante=PhotoImage(file='volante_d_90.png')
+            L_volante=Label(root,image=volante).place(x=1202,y=0)
+            
+            x+=1
+        
+def thread_girar_derecha():
+    p=threading.Thread(target=girar_derecha)
+    p.start()
+
+
+    
+### HILO PARA LA POSICION -1 ####      
+def girar_izquierda():
+        global volante,girar_I
+        x=0
+        while(x<1):
+            girar_I=PhotoImage(file='girar_izquierda_press.png')
+            girar_izquierda=Button(root,bd=0,image=girar_I,command=lambda: thread_girar_izquierda()).place(x=1203,y=600)
+            volante=PhotoImage(file='volante_0.png')
+            L_volante=Label(root,image=volante).place(x=1202,y=0)
+            time.sleep(0.5)
+            volante=PhotoImage(file="volante_i_50.png")
+            L_volante=Label(root,image=volante).place(x=1202,y=0)
+            time.sleep(0.5)
+            volante=PhotoImage(file='volante_i_90.png')
+            L_volante=Label(root,image=volante).place(x=1202,y=0)
+            girar_I=PhotoImage(file='girar_izquierda.png')
+            girar_izquierda=Button(root,bd=0,image=girar_I,command=lambda: thread_girar_izquierda()).place(x=1203,y=600)
+            
+            x+=1
+        
+def thread_girar_izquierda():
+    p=threading.Thread(target=girar_izquierda)
+    p.start()
+
+
+
+
+
+### HILO PARA LA POSICION 0 ####          
+def girar_0():
+        global volante,direccion_0
+        x=0
+        while(x<1):
+            volante=PhotoImage(file='volante_0.png')
+            L_volante=Label(root,image=volante).place(x=1202,y=0)
+            direccion_0=PhotoImage(file='direccion_0_press.png')
+            girar_none=Button(root,bd=0,image=direccion_0,command=lambda: thread_posicion_0()).place(x=1310,y=600)
+            time.sleep(1)
+            direccion_0=PhotoImage(file='direccion_0.png')
+            girar_none=Button(root,bd=0,image=direccion_0,command=lambda: thread_posicion_0()).place(x=1310,y=600)
+            
+            
+            
+            x+=1
+
+def thread_posicion_0():
+    p=threading.Thread(target=girar_0)
+    p.start()
+
+
 
 root.mainloop()
 ################################################################################################## FUNCIONES ##############################################################################
